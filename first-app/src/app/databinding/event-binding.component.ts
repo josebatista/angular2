38a3e5e1c8208dccb8744f779cb475970baa3688
José <jose@Mac-mini-de-Jose.local>
2016-10-28 11:22:24 -0200

    Custom Event Binding
    * modified event binding component to emit an event (string in this case)
    * created a new function into databinding component to show an alert with the message
    * added a custom event into databinding html file on eventbinding selector

--- first-app/src/app/databinding/event-binding.component.ts
+++ first-app/src/app/databinding/event-binding.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'fa-event-binding',
@@ -8,9 +8,10 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class EventBindingComponent {
+  @Output() clicked = new EventEmitter<string>();
 
   onClicked() {
-    alert('It worked!');
+    this.clicked.emit('It Works!');
   }
 
 }
