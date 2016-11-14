28ccb397212f07275ebfe2e149319e2471ecd43a
José <jose@Mac-mini-de-Jose.local>
2016-11-14 15:35:16 -0200

    Loading Components

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ SESSION8
     * Class 69
 SESSION9
   * Class 72
+  * Class 73
--- routes/src/app/home-component.component.ts
+++ routes/src/app/home-component.component.ts
@@ -3,9 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-component',
   template: `
-    <p>
+    <h3>
       home-component Works!
-    </p>
+    </h3>
   `
 })
 export class HomeComponent implements OnInit {
