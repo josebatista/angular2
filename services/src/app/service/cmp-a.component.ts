a7b6938040fb87e59542b29902264b58d6551754
José <jose@Mac-mini-de-Jose.local>
2016-11-14 10:29:03 -0200

    Multiple Instances vs One Instance

--- services/src/app/service/cmp-a.component.ts
+++ services/src/app/service/cmp-a.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { LogService } from './log.service';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'si-cmp-a',
@@ -8,7 +9,7 @@ import { LogService } from './log.service';
   <div>
     <input type="text" #input />
     <button (click)="onLog(input.value)">Log</button>
-    <button (click)="onStore()">Store</button>
+    <button (click)="onStore(input.value)">Store</button>
     <button (click)="onSend()">Send</button>
   </div>
   <hr />
@@ -16,7 +17,7 @@ import { LogService } from './log.service';
     <button (click)="onGet()">Refresh Storage</button>
     <h3>Storage</h3>
     <ul>
-      <li></li>
+      <li *ngFor="let item of items">{{item}}</li>
     </ul>
     <h3>Received Value</h3>
     <p>{{value}}</p>
@@ -27,14 +28,23 @@ import { LogService } from './log.service';
 export class CmpAComponent implements OnInit {
 
   value = '';
+  items: string[] = [];
 
-  constructor(private logService: LogService) { }
+  constructor(private logService: LogService, private dataService: DataService) { }
 
   ngOnInit() {
   }
 
   onLog(value: string) {
     this.logService.writeToLog(value);
+  }
+
+  onStore(value: string) {
+    this.dataService.addData(value);
+  }
+
+  onGet() {
+    this.items = this.dataService.getData().slice(0);
   }
 
 }
