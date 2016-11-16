a463cd97634084ff8d885d24ab21850252fea4d2
José <jose@Mac-mini-de-Jose.local>
2016-11-16 11:20:08 -0200

    Redirecting Request

--- routes/src/app/app.routing.ts
+++ routes/src/app/app.routing.ts
@@ -7,7 +7,8 @@ import { USER_ROUTES } from './user/user.routes';
 const APP_ROUTES: Routes = [
   { path: 'user/:id', component: UserComponent },
   { path: 'user/:id', component: UserComponent, children: USER_ROUTES },
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '/user/1', pathMatch: 'full' }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
