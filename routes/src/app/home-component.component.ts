219e6f0d6b63d7611975c91b8d28bd62b4673948
José <jose@Mac-mini-de-Jose.local>
2016-11-16 09:12:02 -0200

    Extracting Query Params

--- routes/src/app/home-component.component.ts
+++ routes/src/app/home-component.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/RX';
 
 @Component({
   selector: 'app-home-component',
@@ -6,13 +8,26 @@ import { Component, OnInit } from '@angular/core';
     <h1>
       home-component Works!
     </h1>
+    <hr />
+    {{param}}
   `
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subsciption: Subscription
+  param: string;
+
+  constructor(private route: ActivatedRoute) {
+    this.subsciption = this.route.queryParams.subscribe(
+      queryParam => this.param = queryParam['analytics']
+    );
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.subsciption.unsubscribe();
   }
 
 }
