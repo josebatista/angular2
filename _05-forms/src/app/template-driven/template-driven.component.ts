eba2d596107f615b2ef282cc9582907ee27607f0
José <jose@Mac-mini-de-Jose.local>
2016-11-17 12:01:07 -0200

    Class105 -> Submitting a Form and Using the Output (Template-Driven)

--- _05-forms/src/app/template-driven/template-driven.component.ts
+++ _05-forms/src/app/template-driven/template-driven.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'template-driven',
@@ -9,6 +10,10 @@ export class TemplateDrivenComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 
 }
